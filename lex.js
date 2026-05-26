5a5d582aa97f4092abab81bd8256bb0c5d9b8a59
TOGE <cazamirloucas+toge@gmail.com>
2026-05-26 18:32:10 +0300

    Add initial implementation of tokenize function

---
+++ lex.js
@@ -0,0 +1,10 @@
+let func = {'a', 's', 't', 'd', 'r', 'p', 'f', 'c', 'v', };
+tokenize(src) {
+  let pos = 0;
+  let tokens = [];
+  while(pos < src.length) {
+    switch(src[pos]) {};
+  }
+}
+  
+  
