0169826f8b2080bfd91c27d5f7fbffce805c5fea
TOGE <cazamirloucas+toge@gmail.com>
2026-05-27 11:59:24 +0300

    Refactor tokenization variables and functions

--- lex.js
+++ lex.js
@@ -1,15 +1,15 @@
 tokenize(src) {
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const punctuation ={'<': 'LAB', '>': 'RAB',  '.': 'DELIM', '!': 'EOS'};
-  const whitespace = [' ', '\n', '\r', '\t'];
+  const ltrs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const dgts = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const pnct ={'<': 'LAB', '>': 'RAB',  '.': 'DELIM', '!': 'EOS'};
+  const whtspc = [' ', '\n', '\r', '\t'];
   let pos = 0;
-  let tokens = [];
+  let tkns = [];
   const char = src[pos];
   let col = 1;
   let line = 1;
   function move(n = 1) {pos += n; col += n};
-  function token(a, b, c, d = line, e = col) {a.push({
+  function tkn(a, b, c, d = line, e = col) {a.push({
     type: b,
     value: c,
     line: d,
@@ -18,24 +18,24 @@ tokenize(src) {
   while(pos < src.length) {
     mainSwitch:
     switch(true) {
-      case char in alphabet:
+      case char in ltrs:
         let next = src[pos+1];
-        if(next === ".") {token(tokens, "TEXT", char); move(); break mainSwitch};
+        if(next === ".") {tken(tkns, "TEXT", char); move(); break mainSwitch};
         let val = ``;
-        while(src[pos] in alphabet && src[pos] in digits) {val += src[pos]; move()};
-        token(tokens, "IDENTIFIER", val);
+        while(src[pos] in ltrs && src[pos] in dgts) {val += src[pos]; move()};
+        tken(tokens, "IDENTIFIER", val);
         break;
-      case char in digits:
+      case char in dgts:
         let val = ``;
         let i = 0;
         while(src[pos] in digits || src[pos] === '.') {if(src[pos] === '.') {i++}; val += src[pos]; move()};
         if(i > 1) {throw new Error("number found at line " + line + ", column " + col " that contains too many decimal points")};
-        token(tokens, "NUMBER", val);
+        tken(tokens, "NUMBER", val);
         break;
-      case char in punctuation:
-        token(tokens, punctuation[char], char);
+      case char in pnct:
+        tken(tokens, punctuation[char], char);
         break;
-      case char in whitespace:
+      case char in whtspc:
         if(char === '\n') {line++; col = 1};
         pos++;
         break;
@@ -43,16 +43,16 @@ tokenize(src) {
         next = src[pos+1];
         switch(next) {
           case 'O':
-            token(tokens, "L_OUTPUT", '_O');
+            tken(tkns, "L_OUTPUT", '_O');
             break;
           case 'R':
-            token(tokens, "L_RETURN", '_R');
+            tken(tkns, "L_RETURN", '_R');
             break;
           case 'S':
-            token(tokens, "L_STACK", '_S');
+            tken(tkns, "L_STACK", '_S');
             break;
           case '.':
-            token(tokens, "TEXT", '_') ;
+            tken(tkns, "TEXT", '_') ;
             break;
           default:
             throw new Error("can't have _ that isn't nor for a line type, nor for regular text");
