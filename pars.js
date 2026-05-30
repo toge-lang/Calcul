4bb4c01b7784c47d0025de86f83975947809cb45
TOGE <cazamirloucas+toge@gmail.com>
2026-05-30 10:22:41 +0300

    Refactor parser functions for improved readability

--- pars.js
+++ pars.js
@@ -1,59 +1,69 @@
 function parser(tokens) {
   let current = 0;
-  function peek() {
-    return tokens[current];
-  };
-  function peekAhead() {
-    return tokens[current+1];
-  };
+  function peek() {return tokens[current]};
+  function peekAhead() {return tokens[current+1]};
   function eat(type) {
     const token = peek();
     if (token.type === type) {current++; return token};
     else {throw new Error("Unexpected token '" + token + "', instead of expected token '" + type + "'.")};
   };
+  function parseFunctionCall() {
+    const funcName = eat("IDENTIFIER");
+    eat("LAB");
+    let args = [];
+    while(peek().type !== "RAB") {
+      args.push(parseArgument());
+      if(peek().type === "NORMAL_DELIM") {eat("NORMAL_DELIM"); args.push(parseArgument())};
+      if(peek().type === "FUNC_DELIM") {eat("FUNC_DELIM"); args.push(parseFunctionCall())};
+    };
+    eat("RAB");
+    return {
+      type: "FunctionCall",
+      name: funcName,
+      arguments: args
+    };
+  };
   function parseArgument() {
     switch(peek().type) {
-      case 'NUMBER':
-        let token = eat("NUMBER");
+      case 'NUMBER': let token = eat("NUMBER");
         return {
           type: "Literal",
           valueType: "Number",
           value: token
         };
         break;
-      case 'TEXT':
-        let token = eat("TEXT");
+      case 'TEXT': let token = eat("TEXT");
         return {
           type: "Literal",
           valueType: "Text",
           value: token
         };
         break;
-      case 'VARIABLE':
-        let token = eat("VARIABLE");
+      case 'VARIABLE': let token = eat("VARIABLE");
         return {
           type: "VariableReference",
           name: token
         };
         break;
-      case 'PARAMETER':
-        let token = eat("PARAMETER");
+      case 'PARAMETER': let token = eat("PARAMETER");
         return {
           type: "ParameterReference",
           name: token
         };
         break; 
-      case 'IDENTIFIER':
-        if(peekAhead().type === 'LPAREN') {return parseFunctionCall()},
+      case 'IDENTIFIER': 
+        if(peekAhead().type === 'LPAREN') {return parseFunctionCall()}
         else {
           let token = eat("IDENTIFIER");
           return {
             type: "Identifier",
             name: token
           };
+        };
         break;
       default:
-        //TODO: complete
+        throw new Error("unrecognized token ' " + peek().value + " ' at line " + peek().line + ", column " + peek().column + ".");
     };
+  };
 };
 module.exports = {parser};
